6bdf771c23180216cb424560009cc4ea4c53def4
Sean Farhat <seanfarhat9@gmail.com>
2021-03-14 16:26:53 -0700

    training w/ tensorboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchaudio
+from torch.utils.tensorboard import SummaryWriter
 from config import hparams, DATASET_DIR
 from preprocess import preprocess
 from utils import weights_init_unif, load_from_checkpoint, save_checkpoint
@@ -16,11 +17,11 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
     print(device)
 
-    # train_dataset = torchaudio.datasets.LIBRISPEECH(DATASET_DIR, url="train-clean-100", download=True)
+    train_dataset = torchaudio.datasets.LIBRISPEECH(DATASET_DIR, url="train-clean-100", download=True)
     # dev_dataset = torchaudio.datasets.LIBRISPEECH(DATASET_DIR, url="dev-clean", download=True)
     test_dataset = torchaudio.datasets.LIBRISPEECH(DATASET_DIR, url="test-clean", download=True)
 
-    # train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=hparams["batch_size"], shuffle=True, collate_fn=preprocess, pin_memory=use_cuda)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=hparams["batch_size"], shuffle=True, collate_fn=preprocess, pin_memory=use_cuda)
 
     # dev_loader = torch.utils.data.DataLoader(dev_dataset, batch_size=hparams["batch_size"], shuffle=True, collate_fn=preprocess, pin_memory=use_cuda)
 
@@ -38,11 +39,13 @@ def main():
     optimizer = torch.optim.Adam(net.parameters(), lr=hparams["ADAM_lr"])
     finetune_optimizer = torch.optim.SGD(net.parameters(), lr=hparams["SGD_lr"], weight_decay=hparams["SGD_l2_penalty"])
 
-    # for epoch in range(1, hparams["epochs"] + 1):
-    #     train(net, train_loader, criterion, optimizer, epoch, device)
-    #     save_checkpoint(net, optimizer, epoch, hparams["activation"], hparams["batch_size"])
+    writer = SummaryWriter()
+    # writer.add_graph(net)
+
+    for epoch in range(hparams["epochs"]):
+        train(net, train_loader, criterion, optimizer, epoch, device, writer)
+        save_checkpoint(net, optimizer, epoch, hparams["activation"], hparams["batch_size"])
     
-        
     # TODO: Where/when to do dev set?
 
     # net, _, _, _ = load_from_checkpoint(net, optimizer, "activation-relu_batch-size-3_epoch-3.pt", device)
--- training.py
+++ training.py
@@ -2,7 +2,7 @@ import torch
 from config import CHECKPOINT_DIR
 from utils import save_checkpoint
 
-def train(model, train_loader, criterion, optimizer, epoch, device):
+def train(model, train_loader, criterion, optimizer, epoch, device, writer):
     model.train()
     data_len = len(train_loader.dataset)
     for batch_num, data in enumerate(train_loader): 
@@ -22,6 +22,8 @@ def train(model, train_loader, criterion, optimizer, epoch, device):
         optimizer.step() 
 
         if batch_num % 100 == 0 or batch_num == data_len:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_num * len(inputs), data_len,
-                    100. * batch_num / len(train_loader), loss.item()))
+            writer.add_scalar("Loss/train", loss.item(), epoch * data_len + batch_num)
+            writer.flush()     
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_num * len(inputs), data_len,
+                100. * batch_num / len(train_loader), loss.item()))
