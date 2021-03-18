13a5d9c6a7c7ef893a2e94d8b6dde052f42e0685
Sean Farhat <seanfarhat9@gmail.com>
2021-03-18 16:42:04 -0700

    progress on animation function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(() => {
     let songName;
     const lyricsUrl = "lyrics.txt";
+    let audio = $("#audio");
+    audio.hide();
 
     function processAlignment(alignJSON) {
 
@@ -8,9 +10,8 @@ $(document).ready(() => {
 
         let transcript = alignJSON.transcript;
         let words = alignJSON.words;
-        // $("#lyrics").html(lyrics);
-        // console.log($("#lyrics").html());
-        console.log(JSON.stringify(transcript));
+        // console.log(JSON.stringify(transcript));
+
         let currOffset = 0;
         words.forEach(word => {
             if (word.startOffset > currOffset) {
@@ -23,26 +24,51 @@ $(document).ready(() => {
                 }
             }
 
-            let printed = transcript.slice(word.startOffset, word.endOffset);
-            $("#lyrics").append(`<span>${printed}</span>`);
+            // Want to access these elements from list in animation function
+            let text = document.createTextNode(transcript.slice(word.startOffset, word.endOffset));
+            let elem = document.createElement("span");
+            elem.appendChild(text);
+            $("#lyrics").append(elem);
+            word.elem = elem;
+
             currOffset = word.endOffset;
+            });
+        
+        window.requestAnimationFrame(() => {
+            highlightWord(words);
+        });
+    };
+
+    function highlightWord(words) {
+        let t = audio.currentTime; // currently undefined
+        console.log(t);
+        let shouldBeHighlighted = words.filter(word => (word.start <= t && word.end >= t));
+        if (shouldBeHighlighted.length > 0) {
+            console.log(shouldBeHighlighted);
+        } 
+
+        window.requestAnimationFrame(() => {
+            highlightWord(words);
         });
     };
 
     $("#submit").click(() => {
         songName = $("#song_name").val();
         console.log(songName);
+        audio.show();
         // $("#lyrics").load(lyricsUrl);
         $.getJSON("align.json", processAlignment); 
     });
     
     // Use $("#audio").currentTime and words["start"] and words["end"] for syncing highlighting
-    $("#audio").on({
-        "play": function () {
-            console.log("Audio playing!");
-        },
-        "pause": function() {
-            console.log("Audio paused!");
-        } 
-    });
+    // $("#audio").on({
+    //     "playing": function () {
+    //         console.log("Audio playing!");
+    //     },
+    //     "pause": function() {
+    //         console.log("Audio paused!");
+    //     } 
+    // });
+
+    // while (audio.duration > 0 && !audio.paused)
 });
