a59902cf9ffb41bec0e0d6ff5b9c42609e1bbb1a
Sean Farhat <seanfarhat9@gmail.com>
2021-03-18 15:25:33 -0700

    loading/displaying lyrics properly

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 $(document).ready(() => {
     let songName;
-    const url = "3005_lyrics.txt"
+    const lyricsUrl = "lyrics.txt";
+
+    function processAlignment(alignJSON) {
+
+        console.log(alignJSON);
+
+        let transcript = alignJSON.transcript;
+        let words = alignJSON.words;
+        // $("#lyrics").html(lyrics);
+        // console.log($("#lyrics").html());
+        console.log(JSON.stringify(transcript));
+        let currOffset = 0;
+        words.forEach(word => {
+            if (word.startOffset > currOffset) {
+                // There is a newline or space
+                let whitespace = transcript.slice(currOffset, word.startOffset);
+                if (whitespace === "\r\n" || whitespace === "\r\n\r\n") {
+                    $("#lyrics").append("<br>");
+                } else {
+                    $("#lyrics").append(whitespace);
+                }
+            }
+
+            let printed = transcript.slice(word.startOffset, word.endOffset);
+            $("#lyrics").append(`<span>${printed}</span>`);
+            currOffset = word.endOffset;
+        });
+    };
+
     $("#submit").click(() => {
         songName = $("#song_name").val();
         console.log(songName);
-        $("#lyrics").load(url)
-    })
+        // $("#lyrics").load(lyricsUrl);
+        $.getJSON("align.json", processAlignment); 
+    });
     
+    // Use $("#audio").currentTime and words["start"] and words["end"] for syncing highlighting
+    $("#audio").on({
+        "play": function () {
+            console.log("Audio playing!");
+        },
+        "pause": function() {
+            console.log("Audio paused!");
+        } 
+    });
 });
